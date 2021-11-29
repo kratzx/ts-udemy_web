070472a58357532cd2144ed487ab5695f6bc271e
Leonardo Avendaño <leo.aven94@gmail.com>
2021-11-28 23:31:39 -0300

    Update

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,32 @@
 import { Eventing } from './Eventing';
-
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 export interface UserProps {
   id?: number;
   name?: string; 
   age?: number
 }
 
+const rootUrl = 'http://localhost:3000';
 
 export class User {
-  events: Eventing = new Eventing();
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps> 
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): (string | number) {
-    return this.data[propName];
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
   }
 }
