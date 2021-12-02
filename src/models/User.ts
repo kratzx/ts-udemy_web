6aee400a5a85b06a4fa73ae63fb0da228de50ed6
Leonardo Avendaño <leo.aven94@gmail.com>
2021-12-01 22:00:36 -0300

    Finish user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 export interface UserProps {
   id?: number;
   name?: string; 
@@ -28,5 +29,32 @@ export class User {
 
   get get() {
     return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if(typeof id !== 'number') {
+      throw new Error('Cannnot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('update');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
