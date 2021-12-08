7c377e3f727623f571ec2b397e36376fbb74fabd
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-12-07 22:21:06 -0300

    Finished

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { UserForm } from "./views/UserForms";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const userForm = new UserForm(
-  document.getElementById('root')
+const users = new Collection('http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
 );
 
-userForm.render();
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
